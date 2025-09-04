42bf885a68335c0a44caaa39a41aa401129681f1
LoveKapibarasan <lovakapibarasan@lovekapibarasan.org>
2025-09-04 12:26:02 +0200

    Auto Commit

--- white_list_5/setting.sh
+++ white_list_5/setting.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # https://sourceware.org/git/?p=glibc.git;a=blob_plain;f=nss/nss_files/files-hosts.c;hb=HEAD
 
+# .so = .dll
+
 gcc -fPIC -shared -o libnss_regex.so.2 nss_regex.c -ldl -Wall
 sudo cp libnss_regex.so.2 /usr/lib/   # or /lib/x86_64-linux-gnu/
 
