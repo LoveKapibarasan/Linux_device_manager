9ecf8601bfa3543d31482bce63b1d82a377ed39f
LoveKapibarasan <lovekapibarasan@lovekapibarasan.org>
2025-09-05 12:11:39 +0200

    Auto Commit

--- patrol/setting.sh
+++ patrol/setting.sh
@@ -5,10 +5,6 @@
 sudo cp patrol.service /etc/systemd/system/patrol.service
 sudo cp patrol.timer /etc/systemd/system/patrol.timer
 
-mkdir -p /opt/patrol
-sudo cp patrol.sh /opt/patrol/patrol.sh
-sudo chmod +x /opt/patrol/patrol.sh
-
 sudo systemctl daemon-reload
 sudo systemctl enable patrol.timer
 sudo systemctl start patrol.timer
