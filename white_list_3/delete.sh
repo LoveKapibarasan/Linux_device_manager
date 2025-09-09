c6f49c1dbd2b227f742a41d7058a49feeac5e2e5
Lovekapibarasan <lovekapibarasan@lovekapibarasan.org>
2025-09-09 16:36:30 +0200

    Auto Commit

--- white_list_3/delete.sh
+++ white_list_3/delete.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
 sudo pihole uninstall
+
+
+#=== NetworkManager ===
+sudo sed -i '/^\s*dns=none\s*$/d' /etc/NetworkManager/NetworkManager.conf
+cat /etc/NetworkManager/NetworkManager.conf
+
+sudo chattr -i /etc/resolv.conf
+sudo rm -f /etc/resolv.conf
+
+#=== systemd-resolved ===
+sudo systemctl enable systemd-resolved
+sudo systemctl start systemd-resolved
+
+sudo systemctl status pihole.service
--- white_list_3/delete_docker.sh
+++ white_list_3/delete_docker.sh
@@ -11,19 +11,6 @@ sudo rm -rf /opt/pihole
 sudo systemctl disable pihole.service
 sudo systemctl stop pihole.service
 
+sudo ./delete.sh
 
-
-#=== NetworkManager ===
-sudo sed -i '/^\s*dns=none\s*$/d' /etc/NetworkManager/NetworkManager.conf
-cat /etc/NetworkManager/NetworkManager.conf
-
-sudo chattr -i /etc/resolv.conf
-sudo rm -f /etc/resolv.conf
-cat /etc/resolv.conf
-
-#=== systemd-resolved ===
-sudo systemctl enable systemd-resolved
-sudo systemctl start systemd-resolved
-
-systemctl status pihole.service
 sudo docker ps
