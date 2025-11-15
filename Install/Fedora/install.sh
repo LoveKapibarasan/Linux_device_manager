e9601cba097c22bfbf004c5de475675c4a120bda
LoveKapibarasan <lovekapibarasan@lovekapibarasan.org>
2025-11-15 15:12:13 +0100

    Fedora

--- Install/Fedora/install.sh
+++ Install/Fedora/install.sh
@@ -5,13 +5,18 @@ sudo dnf update -y && sudo dnf upgrade
 sudo dnf remove firefox
 rm -rf ~/.mozilla
 
-sudo dnf install git -y
+sudo dnf install git git-lfs sqlite3 -y
+
+
+# Pihole
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+curl -sSL https://install.pi-hole.net | bash
 
 # Cursor
 # https://cursor.com/docs/cli/installation
 curl https://cursor.com/install -fsS | bash
 
-# Sublime-Text
+# Sublime-Text(subl)
 # https://www.sublimetext.com/download
 # Issue: 15.11.2025
 # https://github.com/sublimehq/sublime_text/issues/6669
@@ -20,6 +25,7 @@ sudo rpm -i --nodigest sublime*.rpm
 
 # Opera
 # https://linuxcapable.com/install-opera-on-fedora-linux/
+# Alt + p -> Default Search Engine: DuckDuckGo
 sudo rpm --import https://rpm.opera.com/rpmrepo.key
 sudo tee /etc/yum.repos.d/opera.repo <<RPMREPO
 [opera]
