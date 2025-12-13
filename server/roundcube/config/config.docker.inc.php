e9d781b054a00a924fe3253b3b780f1bc61a2504
LoveKapibarasan <lovekapibarasan@lovekapibarasan.org>
2025-12-13 09:30:10 +0100

    Commit

---
+++ server/roundcube/config/config.docker.inc.php
@@ -0,0 +1,10 @@
+<?php
+$config['imap_host'] = [
+  'ssl://imap.mail.us-east-1.awsapps.com' => 'Amazon WorkMail',
+  'ssl://imap.gmail.com'                  => 'Gmail',
+  'ssl://outlook.office365.com'            => 'Outlook',
+];
+
+$config['imap_port'] = 993;
+$config['imap_auth_type'] = null;
+$config['username_domain'] = '';
