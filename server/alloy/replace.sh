a1f33f682c076916b880daac7979128ffef2a855
LoveKapibarasan <tmp@lovekapibarasan.org>
2026-01-14 17:21:55 +0100

    Change hierarchy

--- server/alloy/replace.sh
+++ server/alloy/replace.sh
@@ -2,5 +2,6 @@
 
 source ../.env
 read -p "Enter org ID: " ORG_ID
+export ORG_ID=''
 sed -i "s/LOKI_BASIC_PASSWORD/${LOKI_BASIC_PASSWORD}/g" ./config.alloy
 sed -i "s/ORG_ID/${ORG_ID}/g" ./config.alloy
