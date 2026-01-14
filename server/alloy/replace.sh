04e772920ab2472909ac8d20d84be59aced5e25e
LoveKapibarasan <tmp@lovekapibarasan.org>
2026-01-14 17:14:48 +0100

    Alloy

--- server/alloy/replace.sh
+++ server/alloy/replace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source ../.env
-sed -i "s/LOKI_BASIC_AUTH_PASSWORD/${LOKI_BASIC_AUTH_PASSWORD}/g" ./config.alloy
-sed -i "s/MIMIR_BASIC_AUTH_PASSWORD/${MIMIR_BASIC_AUTH_PASSWORD}/g" ./config.alloy
+read -p "Enter org ID: " ORG_ID
+sed -i "s/LOKI_BASIC_PASSWORD/${LOKI_BASIC_PASSWORD}/g" ./config.alloy
 sed -i "s/ORG_ID/${ORG_ID}/g" ./config.alloy
