97f6dfe6bf99b1adfb79569b4199f861a5e433ed
LoveKapibarasan <tmp@lovekapibarasan.org>
2026-01-14 17:24:37 +0100

    Fixed password

--- server/alloy/replace.sh
+++ server/alloy/replace.sh
@@ -3,5 +3,5 @@
 source ../.env
 read -p "Enter org ID: " ORG_ID
 export ORG_ID=''
-sed -i "s/LOKI_BASIC_PASSWORD/${LOKI_BASIC_PASSWORD}/g" ./config.alloy
+sed -i "s/LOKI_PASSWORD/${LOKI_PASSWORD}/g" ./config.alloy
 sed -i "s/ORG_ID/${ORG_ID}/g" ./config.alloy
